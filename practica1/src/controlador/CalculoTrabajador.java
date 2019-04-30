d34d94f5f6d995cd442bebf11a7f0d198fd83cf6
jmartb02 <jmartb02@estudiantes.unileon.es>
2019-04-30 20:11:30 +0200

    formato legible

--- practica1/src/controlador/CalculoTrabajador.java
+++ practica1/src/controlador/CalculoTrabajador.java
@@ -19,46 +19,49 @@ public class CalculoTrabajador {
     this.Trabajador=Trabajador;
     }
 //metodo que devuelve el salario base en bruto mensual
-    Double calculoBase(int index){
-   ArrayList brutos = parametro.getSalariobase();
-   Double bruto=  (Double) brutos.get(index);
-  return bruto/14;
+   Double calculoBase(int index){
+        ArrayList brutos = parametro.getSalariobase();
+        Double bruto=  (Double) brutos.get(index);
+        return bruto/14;
     }
  //metodo para obtener el index para operar dependiendo de la categoria del trbaajador   
     int calculoIndex(){
-   int index=0;
-    Double bruto=0.0;
-   String categoria= Trabajador.getCategorias().getNombreCategoria();
-   if(parametro.getCategorias().contains(categoria)){//para comprobar que existe la categoria
-   index =parametro.getCategorias().indexOf(categoria);
-    }else{System.out.println("error al leer la categoria");}
+        int index=0;
+        Double bruto=0.0;
+        String categoria= Trabajador.getCategorias().getNombreCategoria();
+        if(parametro.getCategorias().contains(categoria)){//para comprobar que existe la categoria
+            index =parametro.getCategorias().indexOf(categoria);
+        }else{
+            System.out.println("error al leer la categoria");
+        }
    return index;
     }
+    
     Double calculoComplemento(){
-    Double complemento = parametro.getComplementos().get(calculoIndex())/14;
-    return complemento;
+        Double complemento = parametro.getComplementos().get(calculoIndex())/14;
+        return complemento;
     }
     Double calculoAntiguedad(Trabajadorbbdd trabajador){
         //Extraemos el año y obtenemos los trienios
-       Date alta = trabajador.getFechaAlta();
-      int yearNomina=2019;
-      String year = alta.toString().substring(alta.toString().length()-4);
-      int aux = Integer.parseInt(year);
-      int trienios = (yearNomina-aux)/3;
+        Date alta = trabajador.getFechaAlta();
+        int yearNomina=2019;
+        String year = alta.toString().substring(alta.toString().length()-4);
+        int aux = Integer.parseInt(year);
+        int trienios = (yearNomina-aux)/3;
         Double antiguedad = parametro.getTrienio().get(trienios-1);;
-    return antiguedad;
+        return antiguedad;
     }
     
     Double calculoProrateo(Trabajadorbbdd trabajador){
-    Double resultado=0.0;
+        Double resultado=0.0;
+        
     if(trabajador.getProrateo().equals("SI")){
         //calculo salario base y complementp
-      Double complemento = parametro.getComplementos().get(calculoIndex())/14;
-      Double salarioBase= calculoBase(calculoIndex());//dado por el index de categoria
-      Double antiguedad = calculoAntiguedad(trabajador);
-    resultado=(salarioBase+complemento+antiguedad)*2/12;
+        Double complemento = parametro.getComplementos().get(calculoIndex())/14;
+        Double salarioBase= calculoBase(calculoIndex());//dado por el index de categoria
+        Double antiguedad = calculoAntiguedad(trabajador);
+        resultado=(salarioBase+complemento+antiguedad)*2/12;
     }
     return resultado;
-    
     }
 }
